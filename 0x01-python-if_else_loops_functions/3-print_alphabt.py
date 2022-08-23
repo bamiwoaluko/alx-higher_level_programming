7c68b085933f3c8046604af07f143d923e30ea54
Bamiwo <olalekanaluko123@gmail.com>
2022-08-23 09:35:02 -0700

    fixed errors

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 for i in range(97, 123):
-    if i == 101 or i == 133:
-        continue
-    print("{}".format(chr(i)), end="")
+    if i != 101 and i != 133:
+        print("{}".format(chr(i)), end="")
