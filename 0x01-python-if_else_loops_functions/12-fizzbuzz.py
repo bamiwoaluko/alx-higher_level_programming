7cc6b2b4267ce7e720788295d5a75dcd35e6ed1f
Bamiwo <olalekanaluko123@gmail.com>
2022-08-31 11:29:15 -0700

    fizzbuzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def fizzbuzz():
+    for c in range (1, 101):
+        if c % 3 == 0:
+            print("Fizz" , end="")
+        elif c % 5 == 0:
+            print("Buzz" , end="")
+        elif c % 3 == 0  and c % 5 == 0:
+            print("FizzBuzz" , end="")
+        else:
+            print("{} ".format(c), end="")
+
