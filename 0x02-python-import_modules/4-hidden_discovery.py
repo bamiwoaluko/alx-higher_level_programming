1a0d466e9a53fb4cda69f0fb9ee17e79d80e1450
Bamiwo <olalekanaluko123@gmail.com>
2022-09-03 10:29:54 -0700

    corrected error

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,6 +3,6 @@ if __name__ == "__main__":
     import hidden_4
 
     i = dir(hidden_4)
-    if i[-2:] != "__"
-        print("{}".format(i))
-
+    for r in i:
+        if r[:2] != "__":
+            print("{}".format(r))
