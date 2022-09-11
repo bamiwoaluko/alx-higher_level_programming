2c36daec0860b596b7082c3c51b1d69b23a4abd1
Bamiwo <olalekanaluko123@gmail.com>
2022-09-11 14:45:17 -0700

    completed tasks

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    new = my_list.copy()
+    if idx < 0:
+        return new
+    elif idx > len(my_list):
+        return new
+    else:
+        my_list[idx] = element
+        return my_list
