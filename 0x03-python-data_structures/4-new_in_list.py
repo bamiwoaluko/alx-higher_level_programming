322d326e1a40ae270c804769f28c383970d2aed5
Bamiwo <olalekanaluko123@gmail.com>
2022-09-11 14:48:00 -0700

    completed tasks

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,9 +2,9 @@
 def new_in_list(my_list, idx, element):
     new = my_list.copy()
     if idx < 0:
-        return new
-    elif idx > len(my_list):
-        return new
-    else:
-        my_list[idx] = element
         return my_list
+    elif idx > len(my_list):
+        return my_list
+    else:
+        new[idx] = element
+        return new
