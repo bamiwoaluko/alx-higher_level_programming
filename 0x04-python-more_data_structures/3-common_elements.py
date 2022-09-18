a19ef6c50562aebe4816194cfe6b01a9fa0adf3d
Bamiwo <olalekanaluko123@gmail.com>
2022-09-18 14:32:44 -0700

    made changes to code

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    res = {}
-    for element in set_1:
-        if element in set_2:
-            res = element
-    return res
+    return set_1 & set_2
