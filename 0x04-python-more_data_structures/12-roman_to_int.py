a66d0ad6f1525e5524583dcd48faecd7d1e66f3e
Bamiwo <olalekanaluko123@gmail.com>
2022-09-20 13:20:56 -0700

    roman to integer

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if type(roman_string) is not str or roman_string is None:
+        return 0
+    r_dict = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000}
+    l = [r_dict[x] for x in roman_string]
+    output = 0
+    for i in range(len(l)):
+        output += l[i]
+        if l[i - 1] < l[i] and i != 0:
+            output -= (l[i - 1] + l[i - 1])
+    return output
