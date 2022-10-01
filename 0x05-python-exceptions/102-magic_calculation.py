de043655d8174b3de83f2b5825e2858f0fa9f14c
Bamiwo <olalekanaluko123@gmail.com>
2022-10-01 07:07:56 -0700

    fixed issue

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -3,7 +3,7 @@ def magic_calculation(a, b):
     result = 0
     for i in range(1, 3):
         try:
-            if a > i:
+            if a < i:
                 raise Exception('Too far')
             else:
                 result = result + (a**b)/i
